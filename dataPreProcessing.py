acd108bf367ca6838762e638b3f805f3bbf9ab72
Guilherme Jácome de Paula <guilhermejp4@gmail.com>
2018-04-23 00:31:05 -0300

    Encoding non-numerical data into numbers

--- dataPreProcessing.py
+++ dataPreProcessing.py
@@ -2,11 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#Import dataset and store it in two separate vectors
 dataset = pd.read_csv('data/Data.csv')
 x = dataset.iloc[:, :-1].values #Matrix of features
 y = dataset.iloc[:, 3].values #Dependent variables set
 
+#Treat missing fields replacing them by the row's mean
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN',strategy = 'mean', axis = 0) #Defines the imputer strategy
 imputer = imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3]) #Replaces the missing data
+
+#Encoding non numerical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+le_x = LabelEncoder()#Associates country names to numerical labels
+x[:, 0] = le_x.fit_transform(x[:, 0])#Applies the transformation to the first column only
+
+ohe = OneHotEncoder(categorical_features=[0])#Dummy variables, prevents the algorithm to order the data as for ex France > Germany
+x = ohe.fit_transform(x).toarray()
+
+le_y = LabelEncoder()#Associates country names to numerical labels
+y = le_y.fit_transform(y)#Applies the transformation to the first column only
+
+print(x)
+print(y)
--- dataPreProcessing.r
+++ dataPreProcessing.r
@@ -7,3 +7,14 @@ dataset$Age = ifelse(is.na(dataset$Age), #Verifies if the field age is empty
 dataset$Salary = ifelse(is.na(dataset$Salary), #Verifies if the field age is empty
                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),#if the condition is true, replace by columns mean
                     dataset$Salary)#if the condition is false keep the value
+
+#Encoding non numerical data
+dataset$Country = factor(dataset$Country,
+                        levels = c('France','Spain','Germany'),
+                        labels = c(1, 2, 3))
+
+dataset$Purchased = factor(dataset$Purchased,
+                        levels = c('No','Yes'),
+                        labels = c(0, 1))
+
+print(dataset)
