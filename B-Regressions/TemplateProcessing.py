fbba1b8f60442b635ea1b661f26390057960eca4
Guilherme Jácome <guilhermejp4@gmail.com>
2018-04-28 11:48:33 -0300

    Completed simple linear regression wit python

--- B-Regressions/TemplateProcessing.py
+++ B-Regressions/TemplateProcessing.py
@@ -19,6 +19,36 @@ x_test = sc_x.transform(x_test)
 print(x_train)
 print(x_test)"""
 
+#Fitting Simple linear regression for the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+
+#This line learns the correlation between the features and dependant data
+regressor.fit(x_train, y_train)# features x/dependat variables y
+
+#Predicting the Test set results
+y_pred = regressor.predict(x_test)#Creates a vector of predictions
+ 
+#Visualizing the training set result
+plt.scatter(x_train, y_train, color = 'red')#plots the points of the training set in red
+plt.plot(x_train, regressor.predict(x_train), color = 'blue')#plots the predicted regression y points
+plt.title('Salary vs Experience (TRAINING SET)')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualizing the test set results
+plt.scatter(x_test, y_test, color = 'red') #plots the points of the training set in red
+plt.plot(x_train, regressor.predict(x_train), color = 'green') #plots the predicted regression y points
+plt.title('Salary vs Experience (TEST SET)')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+# print(y_test)
+# print(y_pred)
+
 # print(x)
 # print(y)
 # print(x_train)
